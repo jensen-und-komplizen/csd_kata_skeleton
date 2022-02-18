c01c4ba1dbf47d354fe33b606dbe99da3a6fc25f
Sven Harmstorf <sven@harmstorf.de>
2022-02-18 10:26:20 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/java/KataTest.java
+++ src/test/java/KataTest.java
@@ -17,6 +17,16 @@ public class KataTest {
         assertEquals("N", rot13("A"));
     }
 
+    @Test
+    public void uppercaseMTranslatesToUppercaseZ() {
+        assertEquals("Z", rot13("M"));
+    }
+
+    @Test
+    public void uppercaseNTranslatesToUppercaseA() {
+        assertEquals("A", rot13("N"));
+    }
+
     private String rot13(String stringToConvert) {
         char character = stringToConvert.charAt(0);
         if (character >= 'A' && character <= 'N') {
