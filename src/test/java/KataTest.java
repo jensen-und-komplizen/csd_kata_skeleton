fd100dcadd0f3bcb3a75dde91207e0f4e74d6772
Niklas Rueber <nrueber@igus.net>
2023-10-25 11:12:46 +0200

    add example tests

--- src/test/java/KataTest.java
+++ src/test/java/KataTest.java
@@ -1,9 +1,17 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.Assert.*;
 
 public class KataTest {
     @Test
-    public void firstTest() {
+    public void ExampleTest(){
         assertTrue(false);
+    }
+    @ParameterizedTest
+    @ValueSource(booleans = {false,true})
+    public void AnotherExampleTest(boolean input){
+        assertTrue(input);
     }
 }
