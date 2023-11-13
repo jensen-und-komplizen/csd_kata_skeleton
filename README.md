f9a78534932c229b554466e486829031f80435f2
Björn Jensen <mirouhh@googlemail.com>
2023-11-13 12:06:24 +0100

    Updating junit to version 5.x

---
+++ README.md
@@ -0,0 +1,5 @@
+# Katas
+A very important skill to improve our skills as developer is to perform **Code Katas**. The main objective of a Code Kata is really focused to sharpen and **not** to solve 
+the programming challenge.
+
+The current repository contains source code and test code to make it easier for the participants to do a kata.
--- java/src/test/java/KataTest.java
+++ java/src/test/java/KataTest.java
@@ -1,9 +1,19 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.Assert.*;
 
 public class KataTest {
+
     @Test
-    public void firstTest() {
-        assertTrue(false);
+    public void exampleTest(){
+            assertTrue(false);
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {false,true})
+    public void anotherExampleTest(boolean input){
+        assertTrue(input);
     }
 }
